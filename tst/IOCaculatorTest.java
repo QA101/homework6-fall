1970f1eac6f333072af1ad7c1c0400650d047820
Dustin <dpaulkinney@gmail.com>
2017-11-28 00:29:45 -0800

    Final Update to tests

--- tst/IOCaculatorTest.java
+++ tst/IOCaculatorTest.java
@@ -1,28 +1,46 @@
 package tst;
 import static org.junit.Assert.*;
 
-import org.junit.Test;
+import org.junit.*;
 
 import src.IOCalculator;
 
 public class IOCaculatorTest {
+	double expected;
+	double actual;
+	static IOCalculator io;
+	
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+		String[] args = {"./resources/input.txt"};
+		io = new IOCalculator(args);
+	}
 
 	@Test
-	public void testCalculate() throws Exception {
-		String[] args = {"./resources/input.txt"};
-		IOCalculator io = new IOCalculator(args);
-		assertTrue(io.Calculate(2, 4, "+") == 6);
-		assertTrue(io.Calculate(2, 4, "-") == -2);
-		assertTrue(io.Calculate(2, 4, "*") == 8);
+	public void CalculateTest() throws Exception {
 
-		//This test is incorrect. It should be 0.5 !0.0
-		assertTrue(io.Calculate(2, 4, "/") == 0);
+		expected = 6;
+		actual = io.Calculate(2, 4, "+");
+		assertTrue("Expected: " + expected + " Actual: "+actual, actual == expected);
+		
+		
+		expected = -2;
+		actual = io.Calculate(2, 4, "-");
+		assertTrue("Expected: " + expected + " Actual: "+actual, actual == expected);
+		
+		expected = 8;
+		actual = io.Calculate(2, 4, "*");
+		assertTrue("Expected: " + expected + " Actual: "+actual, actual == expected);
+		
+		
+		//This test will fail due to a bug. It should be 0.5 ! 0.0
+		expected = 0.5;
+		actual = io.Calculate(2, 4, "/");
+		assertTrue("Expected: " + expected + " Actual: "+actual, actual == expected);
 	}
 	
 	@Test (expected = Exception.class)
-	public void testCalculateNegatives() throws Exception {
-		String[] args = {"./resources/input.txt"};
-		IOCalculator io = new IOCalculator(args);
+	public void CalculateNegativesCases() throws Exception {
 		assertTrue(io.Calculate(2, 4, "") == 6);
 	}
 
